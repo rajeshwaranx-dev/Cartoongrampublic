cdf0aa7c0dbec100d07eadd7463b6d79391b891c
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-03-07 22:52:02 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -53,7 +53,7 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("📥 Get File", url=link)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Get File", url=link)]])
 
     await reply_text.edit(f"<b>Here is your link :</b>\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
     if not DISABLE_CHANNEL_BUTTON:
@@ -83,7 +83,7 @@ async def new_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("📥 Get File", url=link)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Get File", url=link)]])
 
     try:
         await message.edit_reply_markup(reply_markup)
